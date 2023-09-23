a35f4310ec9eb0dc19fa65bf0376aeb5d96b18fb
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-09-23 16:02:42 -0300

    Tela de Splash com a navegação para a tela de Home pronta

--- src/screens/splash_screen.tsx
+++ src/screens/splash_screen.tsx
@@ -1,9 +1,32 @@
-import React, {ReactElement} from 'react';
+import {ParamListBase} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import React, {ReactElement, useEffect} from 'react';
 import {Image, View} from 'react-native';
 
 const icon = require('../../assets/images/icon.png');
 
-export default function SplashScreen(): ReactElement {
+export default function SplashScreen({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<ParamListBase, 'SplashScreen'>;
+}): ReactElement {
+  async function loadApplication() {
+    setTimeout(() => {
+      navigation.reset({
+        index: 0,
+        routes: [
+          {
+            name: 'HomeScreen',
+          },
+        ],
+      });
+    }, 4000);
+  }
+
+  useEffect(() => {
+    loadApplication();
+  }, []);
+
   return (
     <View className="w-full h-full bg-[#F3F3F4] items-center justify-center">
       <Image className="w-[60vw] h-[60vw]" source={icon}></Image>
